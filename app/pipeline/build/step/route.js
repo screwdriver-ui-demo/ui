93963b0613bef97c93091d92370038879498ddd6
Alan Dong <alandong2016@gmail.com>
2020-01-30 22:42:22 -0800

    fix: add timer to step route

--- app/pipeline/build/step/route.js
+++ app/pipeline/build/step/route.js
@@ -1,4 +1,9 @@
+import { on } from '@ember/object/evented';
 import Route from '@ember/routing/route';
+import { set, get } from '@ember/object';
+import { getActiveStep } from 'screwdriver-ui/utils/build';
+
+const DEFAULT_TIMER = 800;
 
 export default Route.extend({
   routeAfterAuthentication: 'pipeline.build',
@@ -8,6 +13,7 @@ export default Route.extend({
     // return parent route model
     return this.modelFor('pipeline.build');
   },
+
   afterModel(model) {
     if (!model) {
       return;
@@ -19,6 +25,37 @@ export default Route.extend({
       this.transitionTo('pipeline.build', model.pipeline.get('id'), model.build.get('id'));
     }
   },
+
+  startActiveStep: on('activate', function startTimer() {
+    console.log('activate startActiveStep');
+
+    const model = this.modelFor('pipeline.build');
+    const timer = setInterval(() => {
+      console.log('timer called');
+
+      const name = getActiveStep(get(model, 'build.steps'));
+
+      if (name) {
+        this.transitionTo(
+          'pipeline.build.step',
+          model.pipeline.get('id'),
+          model.build.get('id'),
+          name
+        );
+      } else {
+        console.log('remove timer');
+        clearInterval(this.timer);
+      }
+    }, DEFAULT_TIMER);
+
+    set(this, 'timer', timer);
+  }),
+
+  stopActiveStep: on('deactivate', function clearTimer() {
+    console.log('deactivate stopActiveStep');
+
+    clearInterval(this.timer);
+  }),
 
   actions: {
     didTransition() {
